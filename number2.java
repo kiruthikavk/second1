2bbe5790624b4894b36fc8ecfcff18a3014f8ee7
Murali8344771858 <muraliprawin@gmail.com>
2022-02-04 12:31:41 +0530

    myfile

---
+++ number2.java
@@ -0,0 +1,23 @@
+import java.io.*;
+class number2
+{
+public static void main(String args[])
+{
+for(int i=0;i<=4;i++)
+{
+for(int j=0;j<=i;j++)
+{
+System.out.print( " "+"*");
+}
+System.out.println();
+}
+for(int k=0;k<=4;k++)
+{
+for(int l=4;l>=k;l--)
+{
+System.out.print(" "+"*");
+}
+System.out.println();
+}
+}
+}
